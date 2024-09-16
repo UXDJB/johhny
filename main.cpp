69403a7eb1467e3707ebfcb87cf123dd0ec1459e
UXDJB <jonathan.omar.bowman@gmail.com>
2024-09-16 13:49:44 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// Author's name: Jonathan Bowman
+// Date due: 09/12/2024 at 11:59 pm
+// Date submitted: 09/12/2024
+// Professor: Dr. Baas
+// Class name: CS1
+// Description: This code takes the names inputted by the user.Sorts the names to find the median by using lexicographical ordering.
+
+# include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!" << endl;
+
+    return 0;
+
+}
+
+
